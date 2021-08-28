6019834b8dd6027c10e836786a6194d2d40fbce8
Tee Guang Ying Marcus <guangyingtgy@live.com>
2021-08-28 14:34:28 +0800

    Added code for B2C.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# aadb2c-reactjs
-Sample code for Azure AD B2C &amp; React JS
+# Azure AD B2C Quick Start with React JS
+Sample code for Azure AD B2C &amp; React JS. This is using default React JS template, with Azure AD B2C. Full technical description can be found in my Medium Blog: [Azure AD B2C Quick Start for React JS Single Page Apps (SPA)](https://marcustee.medium.com/)
--- aadb2c-app/src/index.js
+++ aadb2c-app/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "./authConfig";
+
+export const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App msalInstance={msalInstance}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
